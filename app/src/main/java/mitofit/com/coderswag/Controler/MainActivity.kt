0ef0143da967533cf93ff1b365acd63ee0dc8802
Amin nasseri <aminnaaseri@gmail.com>
2019-10-14 01:12:12 +0330

    add dataa for products

--- app/src/main/java/mitofit/com/coderswag/Controler/MainActivity.kt
+++ app/src/main/java/mitofit/com/coderswag/Controler/MainActivity.kt
@@ -2,12 +2,25 @@ package mitofit.com.coderswag.Controler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import mitofit.com.coderswag.Model.Category
 import mitofit.com.coderswag.R
+import mitofit.com.coderswag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+        categoryListView.adapter =adapter
+
+
     }
 }
--- app/src/main/java/mitofit/com/coderswag/Model/Category.kt
+++ app/src/main/java/mitofit/com/coderswag/Model/Category.kt
@@ -2,4 +2,8 @@ package mitofit.com.coderswag.Model
 
 import android.icu.text.CaseMap
 
-class Category(val title:String,val image:String)
+class Category(val title:String,val image:String){
+    override fun toString(): String {
+        return title
+    }
+}
